c17a8bb808f6a62b108c0d91ee8d69e23a323e83
kimhaun <wha02068@gmail.com>
2021-03-14 15:54:21 +0900

    호스트 주소 작성 + 로그인 함수 기초 구조 작성

--- app/src/main/java/kr/co/neoplus/serverapp_daily10minutes_20210314/utils/ServerUtil.kt
+++ app/src/main/java/kr/co/neoplus/serverapp_daily10minutes_20210314/utils/ServerUtil.kt
@@ -2,4 +2,14 @@ package kr.co.neoplus.serverapp_daily10minutes_20210314.utils
 
 class ServerUtil {
 
+    val HOST_URL = "http://15.164.153.174"
+
+    companion object {
+
+        fun postRequestLogin(){
+
+        }
+
+    }
+
 }
