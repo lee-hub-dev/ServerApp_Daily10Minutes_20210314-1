e33797d0ab305c0693a081cdaa97f67175cd9e44
kimhaun <wha02068@gmail.com>
2021-03-14 16:10:49 +0900

    POST - FormData 에 로그인 필요 데이터 첨부

--- app/src/main/java/kr/co/neoplus/serverapp_daily10minutes_20210314/utils/ServerUtil.kt
+++ app/src/main/java/kr/co/neoplus/serverapp_daily10minutes_20210314/utils/ServerUtil.kt
@@ -1,14 +1,21 @@
 package kr.co.neoplus.serverapp_daily10minutes_20210314.utils
 
+import okhttp3.FormBody
+
 class ServerUtil {
 
     companion object {
 
         val HOST_URL = "http://15.164.153.174"
 
-        fun postRequestLogin() {
+        fun postRequestLogin(id : String, pw : String) {
 
             val urlString = "${HOST_URL}/user"
+
+            val formData = FormBody.Builder()
+                .add("email", id)
+                .add("password", pw)
+                .build()
 
         }
 
