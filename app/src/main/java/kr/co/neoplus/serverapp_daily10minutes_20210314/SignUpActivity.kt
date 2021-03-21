23b736441c88c9e3f58bd2fa1f128847ae69adc3
kimhaun <wha02068@gmail.com>
2021-03-21 10:02:21 +0900

    회원가입 화면 추가

--- app/src/main/java/kr/co/neoplus/serverapp_daily10minutes_20210314/SignUpActivity.kt
+++ app/src/main/java/kr/co/neoplus/serverapp_daily10minutes_20210314/SignUpActivity.kt
@@ -1,12 +1,22 @@
 package kr.co.neoplus.serverapp_daily10minutes_20210314
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class SignUpActivity : BaseActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
 
     }
+
+    override fun setValues() {
+
+    }
+
 }
